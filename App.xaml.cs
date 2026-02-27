a8a4118573bb957a4bae813bd49e969b93138e47
bh95ox <bh95ox@student.sunderland.ac.uk>
2026-02-27 10:30:26 +0000

    MVVM folder structure

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,7 +2,7 @@
 using System.Data;
 using System.Windows;
 
-namespace VetPrescriptionKiosk_sourceCode
+namespace VetPrescriptionKiosk
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace VetPrescriptionKiosk_sourceCode
+namespace VetPrescriptionKiosk
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
